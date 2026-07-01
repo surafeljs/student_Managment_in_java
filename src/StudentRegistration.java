0b191d7ea0436d4785c4356124f4fcf7752b8d62
surafeljs <surafel.node@gmail.com>
2026-07-01 06:21:22 -0700

    fis timestamp

--- src/StudentRegistration.java
+++ src/StudentRegistration.java
@@ -1,13 +1,14 @@
 import java.sql.Timestamp;
 import java.sql.Date;
 import java.util.Scanner;
+import java.util.UUID;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class StudentRegistration {
        Scanner scanner =new Scanner(System.in);
-
+UUID uuid =UUID.randomUUID();
    Timestamp ts = new Timestamp(System.currentTimeMillis());
    
     public void studentRegistration(){
@@ -35,18 +36,13 @@ INSERT INTO registration(
     email_address,
     class,
     admission_date,
-    department,
     section
 )
-VALUES(?,?,?,?,?,?,?,?,?,?,?)
+VALUES(?,?,?,?,?,?,?,?,?,?)
 """;
       //   con.setAutoCommit(false);
         
    PreparedStatement ps=con.prepareStatement(sql);
-
-System.out.println("Student Id");
-   
-String student_id = scanner.nextLine();
 
 System.out.println("Student First Name");
 String student_first_name = scanner.nextLine();
@@ -57,7 +53,7 @@ String student_last_name = scanner.nextLine();
 System.out.println("Gender");
 String gender = scanner.nextLine();
 
-System.out.println("Date of Birth");
+System.out.println("Date of Birth (yyyy-mm-dd)");
 String date_of_birth = scanner.nextLine();
 
 System.out.println("Phone Number");
@@ -69,32 +65,29 @@ String email_address = scanner.nextLine();
 System.out.println("Class");
 String class_name = scanner.nextLine();
 
-System.out.println("Admission Date");
-String admission_date = scanner.nextLine();
-
-System.out.println("Department");
-String department = scanner.nextLine();
-
 System.out.println("Section");
-int section = scanner.nextInt();
+String section = scanner.nextLine();
 
 
+// Convert String date to SQL Date
+Date date = Date.valueOf(date_of_birth);
 
 
-Date date=Date.valueOf(date_of_birth);
-Date admissionDate = Date.valueOf(admission_date);
+// Current admission date
+Timestamp ts = new Timestamp(System.currentTimeMillis());
 
-ps.setString(1, student_id);
-ps.setString(2,student_first_name);
+
+// PreparedStatement
+ps.setString(1, uuid.toString());
+ps.setString(2, student_first_name);
 ps.setString(3, student_last_name);
 ps.setString(4, gender);
 ps.setDate(5, date);
 ps.setString(6, phone_number);
 ps.setString(7, email_address);
 ps.setString(8, class_name);
-ps.setDate(9, admissionDate);
-ps.setString(10, department);
-ps.setInt(11, section);
+ps.setTimestamp(9, ts);
+ps.setString(10, section);
 
 
 int result= ps.executeUpdate();
